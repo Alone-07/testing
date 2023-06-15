bb8f165a8bea0d0f5dc2e6e37fc2c1f0dbc178f3
Alone-07 <al71one2002@gmail.com>
2023-06-15 21:20:08 +0530

    nothing

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 "use strict";
-
-import http from "http";
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
-    res.end();
-  })
-  .listen(3000, (_) => console.log("server is running...."));
