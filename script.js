356e36f576ae20943480ea81710e0b6fec95ea42
Alone-07 <al71one2002@gmail.com>
2023-12-04 20:13:52 +0530

    changed to http

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// import http from 'node:http'
-import https from 'node:https'
+import http from 'node:http'
+// import https from 'node:https'
 import fs from 'node:fs'
 import { default as indexHTML } from './modules/functions.js'
 
@@ -11,13 +11,23 @@ const options = {
 };
 
 
-https.createServer(options, (req, res) => {
+// https.createServer(options, (req, res) => {
+//   if (req.url == '/api') {
+//     res.writeHead(200, { 'Content-Type': 'applications/json' })
+//     res.write('{"name": "krish",}')
+//     res.end()
+//   }
+//   indexHTML(req, res, fs)
+// }).listen(PORT, () => console.log('https server is running on PORT', PORT))
+//
+
+const server = http.createServer((req, res) => {
   if (req.url == '/api') {
     res.writeHead(200, { 'Content-Type': 'applications/json' })
     res.write('{"name": "krish",}')
     res.end()
   }
   indexHTML(req, res, fs)
-}).listen(PORT, () => console.log('https server is running on PORT', PORT))
+})
 
-
+server.listen(PORT, _ => console.log('http server is runnng on port', PORT))
